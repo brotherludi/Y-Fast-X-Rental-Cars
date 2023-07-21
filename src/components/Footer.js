26bf241779ff60fb2f1800f5723ec8a8b4821842
Di Lu <brotherludi@hotmail.com>
2023-07-20 20:59:53 -0600

    Update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Made with ❤️ by: Bruce Lee ⭐️ Di Lu ⭐️ Seaf</footer>;
+  return (
+    <footer>
+      Made with ❤️ by:{" "}
+      <a
+        href="https://github.com/blee77"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ color: "white", textDecoration: "none" }}
+      >
+        Bruce Lee
+      </a>{" "}
+      ⭐️{" "}
+      <a
+        href="https://github.com/brotherludi"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ color: "white", textDecoration: "none" }}
+      >
+        Di Lu
+      </a>{" "}
+      ⭐️{" "}
+      <a
+        href="https://github.com/SEAF39"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ color: "white", textDecoration: "none" }}
+      >
+        Seaf
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
