7a5fe388b5adb0abd5a2dccf6a330aef223354ef
Di Lu <brotherludi@hotmail.com>
2023-08-01 20:49:23 -0600

    Update routes_initial

--- Fast-X-RentalsForSale-api/src/routes/contact.js
+++ Fast-X-RentalsForSale-api/src/routes/contact.js
@@ -1,9 +1,21 @@
-import { Router } from 'express';
-const router = Router();
+// import { Router } from 'express';
+// const router = Router();
 
-// Contact Route
-router.get('/contact', (req, res) => {
-  res.json({ message: 'Contact us at support@fastxrentalsforsale.com' });
+// // Contact Route
+// router.get('/contact', (req, res) => {
+//   res.json({ message: 'Contact us at support@fastxrentalsforsale.com' });
+// });
+
+// export default router;
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+  try {
+    return res.status(200).send({ message: "This is contact 👍🏻" });
+  } catch (error) {
+    console.error("Error fetching cars for sale:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-export default router;
+module.exports = router;
--- Fast-X-RentalsForSale-api/src/routes/loginRegister.js
+++ Fast-X-RentalsForSale-api/src/routes/loginRegister.js
@@ -1,17 +1,40 @@
-import { Router } from 'express';
-const router = Router();
-import { registerUser } from './db/queries';
+//import { Router } from 'express';
+//const router = Router();
+//import { registerUser } from './db/queries';
 
 // Login/Register Route - Register a New User
-router.post('/register', async (req, res) => {
-  const userData = req.body;
+// router.post('/register', async (req, res) => {
+//   const userData = req.body;
+//   try {
+//     const newUser = await registerUser(userData);
+//     res.json(newUser);
+//   } catch (error) {
+//     console.error('Error registering user:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+// export default router;
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
   try {
-    const newUser = await registerUser(userData);
-    res.json(newUser);
+    return res.status(200).send({ message: "This is login/register✅" });
   } catch (error) {
-    console.error('Error registering user:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching cars for sale:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-export default router;
+router.post("/", async (req, res) => {
+  try {
+    return res
+      .status(200)
+      .send({ message: "This is post login/register✅", data: req.body });
+  } catch (error) {
+    console.error("Error fetching cars for sale:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
