3f2c4495a9d7190c4573751cc6371c2fd4f451e5
Di Lu <brotherludi@hotmail.com>
2023-08-07 19:11:06 -0600

    Correct seeds

--- Fast-X-RentalsForSale-api/src/db/schema/seeds.sql
+++ Fast-X-RentalsForSale-api/src/db/schema/seeds.sql
@@ -1,10 +1,10 @@
--- Seed data for users
-INSERT INTO users (first_name, last_name, email, username, password, user_type, location)
+-- Seed data for companies
+INSERT INTO companies (user_id, company_name, rating, location)
 VALUES
-  ( 'SEAF', 'Lastname', 'seaf@example.com', 'seafuser', 'hashed_password', 'user', 'New York'),
-  ( 'DI', 'Lastname', 'di@example.com', 'diuser', 'hashed_password', 'user', 'Los Angeles'),
-  ( 'BRUCE', 'Lastname', 'bruce@example.com', 'bruceuser', 'hashed_password', 'user', 'Chicago'),
-  ( 'ABDUL', 'Lastname', 'abdul@example.com', 'abduluser', 'hashed_password', 'user', 'Houston');
+  ( 1, 'SEAF Car Rentals', 4.5, 'New York'),
+  ( 2, 'DI Car Rentals', 4.2, 'Los Angeles'),
+  ( 3, 'BRUCE Car Rentals', 4.7, 'Chicago'),
+  ( 4, 'ABDUL Car Rentals', 4.1, 'Houston');
 
 -- Seed data for car_listings
 INSERT INTO car_listings (company_id, car_make, car_model, mileage, price, year, color, images, visibility, car_luxury)
@@ -15,13 +15,14 @@ VALUES
   ( 4, 'Chevrolet', 'Malibu', 28000, 19000.00, 2019, 'Silver', ARRAY['image7.jpg', 'image8.jpg'], true, false),
   ( 2, 'Toyota', 'Camry', 15000, 16000.00, 2021, 'Red', ARRAY['image9.jpg', 'image10.jpg'], true, true);
 
--- Seed data for companies
-INSERT INTO companies (user_id, company_name, rating, location)
+-- Seed data for favorites (Ensure that the car_id values match existing entries in car_listings)
+INSERT INTO favorites (user_id, car_id, created_at, updated_at)
 VALUES
-  ( 1, 'SEAF Car Rentals', 4.5, 'New York'),
-  ( 2, 'DI Car Rentals', 4.2, 'Los Angeles'),
-  ( 3, 'BRUCE Car Rentals', 4.7, 'Chicago'),
-  ( 4, 'ABDUL Car Rentals', 4.1, 'Houston');
+  ( 2, 1, NOW(), NOW()),
+  ( 4, 1, NOW(), NOW()),
+  ( 3, 3, NOW(), NOW()), -- Change the car_id to match an existing entry
+  ( 1, 2, NOW(), NOW()), -- Change the car_id to match an existing entry
+  ( 4, 5, NOW(), NOW()); -- Change the car_id to match an existing entry
 
 -- Seed data for reviews
 INSERT INTO reviews (reviewer_id, company_id, rating)
@@ -31,12 +32,3 @@ VALUES
   ( 1, 3, 4.5),
   ( 4, 2, 3.9),
   ( 3, 4, 4.2);
-
--- Seed data for favorites
-INSERT INTO favorites (user_id, car_id, created_at, updated_at)
-VALUES
-  ( 2, 1, NOW(), NOW()),
-  ( 4, 1, NOW(), NOW()),
-  ( 3, 1, NOW(), NOW()),
-  ( 1, 1, NOW(), NOW()),
-  ( 4, 1, NOW(), NOW());
