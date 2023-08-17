ba2f746cd44796c2deba2d8ad5bd3f338762c1ea
Di Lu <brotherludi@hotmail.com>
2023-08-17 14:37:06 -0600

    CSS Update

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "../styles.css";
 
 const Introduction = () => {
   return (
     <div className="introduction">
       <h2>Discover Quality Used Rental Cars</h2>
-      <p>
+      <p className="intro-text">
         Low Mileage, Nearly New Models, and Unbeatable Prices! Find your dream
-        car at a fraction of the cost of buying brand new. Welcome to Fast-X-RentalsForSale, where incredible deals await.
+        car at a fraction of the cost of buying brand new. Welcome to Fast-X-RentalsForSale, where incredible deals await 🤩
       </p>
     </div>
   );
